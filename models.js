6ae1f99461e78079eda57c09145986a8ceedc8d7
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-11-18 11:06:10 +0000

    Added UserSchema in models.js

--- models.js
+++ models.js
@@ -3,3 +3,15 @@ const bcrypt = require('bcrypt');
 const {Schema} = mongoose;
 
 // User Schema
+const UserSchema = new Schema({
+  local: {
+    username: String,
+    password: String
+  },
+  facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
+  }
+});
