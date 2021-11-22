913c1570605155c815f99fe41e7b19c885312bfa
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-11-22 14:07:00 +0000

    Completed serialization and deserialization at passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,20 @@
 const LocalStrategy = require('passport-local').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('../models').User;
+const configAuth = require('./auth.js');
+
 
 module.exports = function(passport) {
   // Serialize and Deserialize
   passport.serializeUser(function(user,done) {
     done(null,user.id);
   });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err,user) {
+      done(err, user);
+    });
+  });
+
+  
 };
