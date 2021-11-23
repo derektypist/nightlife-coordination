eb3abd3e30241480b64232e32f43ad6c56d5619d
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-11-23 14:06:33 +0000

    Completed login local strategy at passport.js

--- config/passport.js
+++ config/passport.js
@@ -45,5 +45,32 @@ module.exports = function(passport) {
     })
   }));
 
+  // Login Local Strategy
+  passport.use('local-login', new LocalStrategy({
+    usernameField: "email",
+    passwordField: "password",
+    passReqToCallback: true
+  }, function(req,email,password,done) {
+    process.nextTick(function() {
+      User.findOne({"local.username": email}, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+
+        if (!user) {
+          return done(null, false, req.flash('loginMessage', 'No User Found'));
+        }
+
+        if (!user.validPassword(password)) {
+          return done(null, false, req.flash('loginMessage', 'Wrong Password'));
+        }
+
+        return done(null,user);
+      });
+    });
+  }));
+
+  
+
   
 };
