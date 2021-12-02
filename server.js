b457547c55bda5ed553802718ad7c6df9d7f1de5
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-12-02 17:35:02 +0000

    Completed app configuration at server.js

--- server.js
+++ server.js
@@ -8,3 +8,23 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const Yelp = require('yelp');
 const app = express();
+
+// App Configuration
+app.set('view engine','pug');
+app.set('views','./views');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(function(req,res,next) {
+  res.locals.user = req.user;
+  next();
+});
